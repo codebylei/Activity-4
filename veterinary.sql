a04ec503a774ae55fcc51a60eeb8034db8ae46e5
Niel <24-30354@g.batstate-u.edu.ph>
2025-11-07 05:06:58 +0000

    feat: create table owners

--- veterinary.sql
+++ veterinary.sql
@@ -1,2 +1,9 @@
 CREATE DATABASE veterinary;
 
+CREATE TABLE owners (
+  ownerid INT PRIMARY KEY AUTO_INCREMENT,
+  firstname VARCHAR(50),
+  lastname VARCHAR(50),
+  address VARCHAR(100),
+  contactnumber VARCHAR(20)
+);

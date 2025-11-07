b3deec5be6bca9c914ae44be60d3d81891afc52c
Niel <24-30354@g.batstate-u.edu.ph>
2025-11-07 05:08:20 +0000

    feat: create table animals

--- veterinary.sql
+++ veterinary.sql
@@ -7,3 +7,14 @@ CREATE TABLE owners (
   address VARCHAR(100),
   contactnumber VARCHAR(20)
 );
+
+CREATE TABLE animals (
+  animalid INT PRIMARY KEY AUTO_INCREMENT,
+  ownerid INT,
+  name VARCHAR(50),
+  species VARCHAR(50),
+  breed VARCHAR(50),
+  gender VARCHAR(10),
+  age INT,
+  FOREIGN KEY (ownerid) REFERENCES owners(ownerid)
+);
